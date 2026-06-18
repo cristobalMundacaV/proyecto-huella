03294e1bc0a85b406a25106dfb7c8f52a38ef5f6
Cristobal Mundaca <cristobal.mundaca1@virginiogomez.cl>
2026-06-18 18:26:36 -0400

    feat(iot): expose sensor serializers

--- backend/apps/iot/serializers.py
+++ backend/apps/iot/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import LecturaSensor
+from .models import DispositivoSensor, LecturaSensor, RegistroSensor
 
 
 class LecturaSensorSerializer(serializers.ModelSerializer):
@@ -25,3 +25,134 @@ class LecturaSensorSerializer(serializers.ModelSerializer):
         if value < 0:
             raise serializers.ValidationError("El valor no puede ser negativo.")
         return value
+
+
+class DispositivoSensorSerializer(serializers.ModelSerializer):
+    api_key = serializers.CharField(write_only=True, required=False, allow_blank=True)
+    constructora_id = serializers.CharField(source="constructora.constructora_id", read_only=True)
+    constructora_nombre = serializers.CharField(source="constructora.nombre", read_only=True)
+    obra_codigo = serializers.CharField(source="obra.codigo_obra", read_only=True)
+    obra_nombre = serializers.CharField(source="obra.nombre", read_only=True)
+    etapa_codigo = serializers.CharField(source="etapa.etapa_id", read_only=True)
+    etapa_nombre = serializers.CharField(source="etapa.nombre", read_only=True)
+    factor_label = serializers.CharField(source="factor_emision_default.actividad", read_only=True)
+
+    class Meta:
+        model = DispositivoSensor
+        fields = [
+            "id",
+            "dispositivo_id",
+            "nombre",
+            "constructora",
+            "constructora_id",
+            "constructora_nombre",
+            "obra",
+            "obra_codigo",
+            "obra_nombre",
+            "etapa",
+            "etapa_codigo",
+            "etapa_nombre",
+            "tipo_sensor",
+            "ubicacion",
+            "descripcion",
+            "factor_emision_default",
+            "factor_label",
+            "api_key",
+            "activo",
+            "metadata",
+            "last_seen_at",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = [
+            "id",
+            "constructora_id",
+            "constructora_nombre",
+            "obra_codigo",
+            "obra_nombre",
+            "etapa_codigo",
+            "etapa_nombre",
+            "factor_label",
+            "last_seen_at",
+            "created_at",
+            "updated_at",
+        ]
+        extra_kwargs = {"constructora": {"write_only": True}}
+
+    def create(self, validated_data):
+        raw_key = validated_data.pop("api_key", None)
+        dispositivo = DispositivoSensor(**validated_data)
+        dispositivo.set_api_key(raw_key)
+        dispositivo.save()
+        return dispositivo
+
+    def update(self, instance, validated_data):
+        raw_key = validated_data.pop("api_key", None)
+        for field, value in validated_data.items():
+            setattr(instance, field, value)
+        if raw_key:
+            instance.set_api_key(raw_key)
+        instance.save()
+        return instance
+
+
+class RegistroSensorSerializer(serializers.ModelSerializer):
+    dispositivo_id = serializers.CharField(source="dispositivo.dispositivo_id", read_only=True)
+    dispositivo_nombre = serializers.CharField(source="dispositivo.nombre", read_only=True)
+    constructora_id = serializers.CharField(source="constructora.constructora_id", read_only=True)
+    constructora_nombre = serializers.CharField(source="constructora.nombre", read_only=True)
+    obra_codigo = serializers.CharField(source="obra.codigo_obra", read_only=True)
+    obra_nombre = serializers.CharField(source="obra.nombre", read_only=True)
+    etapa_codigo = serializers.CharField(source="etapa.etapa_id", read_only=True)
+    etapa_nombre = serializers.CharField(source="etapa.nombre", read_only=True)
+    registro_emision_id = serializers.IntegerField(source="registro_emision.id", read_only=True)
+
+    class Meta:
+        model = RegistroSensor
+        fields = [
+            "id",
+            "external_id",
+            "dispositivo",
+            "dispositivo_id",
+            "dispositivo_nombre",
+            "constructora",
+            "constructora_id",
+            "constructora_nombre",
+            "obra",
+            "obra_codigo",
+            "obra_nombre",
+            "etapa",
+            "etapa_codigo",
+            "etapa_nombre",
+            "tipo",
+            "valor",
+            "unidad",
+            "factor_catalogo",
+            "factor_emision_usado",
+            "co2e_estimado",
+            "timestamp_sensor",
+            "received_at",
+            "estado_procesamiento",
+            "registro_emision",
+            "registro_emision_id",
+            "metadata",
+            "raw_payload",
+            "error_procesamiento",
+        ]
+        read_only_fields = [
+            "id",
+            "dispositivo_id",
+            "dispositivo_nombre",
+            "constructora_id",
+            "constructora_nombre",
+            "obra_codigo",
+            "obra_nombre",
+            "etapa_codigo",
+            "etapa_nombre",
+            "factor_emision_usado",
+            "co2e_estimado",
+            "received_at",
+            "estado_procesamiento",
+            "registro_emision_id",
+            "error_procesamiento",
+        ]
