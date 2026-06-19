7b3a2050fbe42b80086f1ab3696cf2738acb9c46
Cristobal Mundaca <cristobal.mundaca1@virginiogomez.cl>
2026-06-19 10:34:01 -0400

    feat(frontend): add intelligence page

---
+++ frontend/src/features/intelligence/pages/IntelligencePage.jsx
@@ -0,0 +1,7 @@
+import IntelligencePanel from "../components/IntelligencePanel";
+
+function IntelligencePage() {
+  return <IntelligencePanel initialScope="dashboard" />;
+}
+
+export default IntelligencePage;
